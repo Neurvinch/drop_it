826695c676f31da9c4dcbcd59511ba5543d37bf0
neuervinch <npandian515@gmail.com>
2025-04-04 22:01:53 +0530

    Add profile controller with getProfile and updateProfile functions

---
+++ bk/Controllers/profile.js
@@ -0,0 +1,39 @@
+const UserModel = require("../Models/UserModel");
+
+exports.getProfile = async (req, res) => {
+  try {
+    const profile = await UserModel.findOne({
+      username: req.user.username,
+    }).select("email username ");
+
+    if (!profile) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+
+    res.status(200).json({ success: true, data: profile });
+  } catch (error) {
+    console.error("Error in getProfile:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const updateUser = await UserModel.findOneAndUpdate(
+      {
+        username: req.user.username,
+      },
+      req.body,
+      { new: true }
+    );
+
+    if (!updateUser) {
+      return res.status(404).json({ message: "Profile not found" });
+    }
+
+    res.status(200).json({ success: true, data: updateUser });
+  } catch (error) {
+    console.error("Error in updateProfile:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
