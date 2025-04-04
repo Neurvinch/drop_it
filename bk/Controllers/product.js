07b552eb3a08d063ccda0a297ef7d250f204ac63
neuervinch <npandian515@gmail.com>
2025-04-04 22:09:09 +0530

    Add product controller with createProduct and getProducts functions

---
+++ bk/Controllers/product.js
@@ -0,0 +1,44 @@
+const Product = require("../Models/ProductSchema");
+exports.createProduct = async (req, res) => {
+  const { name, description, category, condition, starting_price, image_urls } =
+    req.body;
+
+  try {
+    const product = new Product({
+      name,
+      description,
+      category,
+      condition,
+      seller_id: req.user._id,
+      starting_price,
+      current_price: starting_price,
+      image_urls,
+    });
+    await product.save();
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      data: product,
+    });
+  } catch (error) {
+    console.error("Error in createProduct:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({
+      is_sold: false,
+    }).populate("seller_id", "username ");
+
+    if (!products || products.length === 0) {
+      return res.status(404).json({ message: "No products found" });
+    }
+
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error in getProducts:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
