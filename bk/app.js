c882abca2fc9ebde968f3ae53dc7eda923d9275f
neuervinch <npandian515@gmail.com>
2025-04-04 20:08:42 +0530

    Add cookie-parser middleware and configure server with environment variables

---
+++ bk/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Set CORS options to allow requests from your frontend and include credentials
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+//
+
+app.use("/api");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
